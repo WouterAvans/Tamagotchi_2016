95c5721860bea6e837337e0d710168e3f8711206
Max Beekmans <max.beekmans@hotmail.com>
2016-04-04 13:22:43 +0200

    Initial commit including all libraries

---
+++ Tamagotchi/Tamagotchi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tamagotchi.Startup))]
+namespace Tamagotchi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
