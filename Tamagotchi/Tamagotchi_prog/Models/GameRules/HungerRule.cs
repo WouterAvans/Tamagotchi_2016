ae2ce84d064c56aa9ae8cd61926d42a885f976de
Max Beekmans <max.beekmans@hotmail.com>
2016-04-05 23:37:08 +0200

    added ninject and some classes

---
+++ Tamagotchi/Tamagotchi_prog/Models/GameRules/HungerRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tamagotchi_prog.Models.GameRules
+{
+    public class HungerRule : IGameRule
+    {
+        public Tamagotchi ExecuteRule(Tamagotchi tamagotchi)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
