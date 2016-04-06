525a5f2b07ff1de6b884d2966a32e5ec3bdc6637
Max Beekmans <max.beekmans@hotmail.com>
2016-04-06 14:42:01 +0200

    Implemented all rules only need to check for statuseffect crazy while doing an action

--- Tamagotchi/Tamagotchi_prog/Models/GameRules/IsolationRule.cs
+++ Tamagotchi/Tamagotchi_prog/Models/GameRules/IsolationRule.cs
@@ -7,9 +7,28 @@ namespace Tamagotchi_prog.Models.GameRules
 {
     public class IsolationRule : IGameRule
     {
-        public Tamagotchi ExecuteRule(Tamagotchi tamagotchi)
+        public void ExecuteRule(Tamagotchi tamagotchi, double timePassed, Dictionary<String, int> multipliers)
         {
-            throw new NotImplementedException();
+            tamagotchi.Health += (int) Math.Round(timePassed*multipliers["isolation"]);
+
+            if (tamagotchi.Health <= 20)
+            {
+                tamagotchi.StatusEffects.Add(StatusEffect.Athlete);
+            }
+            else if (tamagotchi.StatusEffects.Contains(StatusEffect.Athlete))
+            {
+                tamagotchi.StatusEffects.Remove(StatusEffect.Athlete);
+            }
+
+            if(tamagotchi.Health >= 100)
+            {
+                tamagotchi.StatusEffects.Add(StatusEffect.Crazy);
+            }
+            else if (tamagotchi.StatusEffects.Contains(StatusEffect.Crazy))
+            {
+                tamagotchi.StatusEffects.Remove(StatusEffect.Crazy);
+            }
+
         }
     }
 }
