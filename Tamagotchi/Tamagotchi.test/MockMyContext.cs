4b2f38acc348281b503976bfff96c1457187722e
Max Beekmans <max.beekmans@hotmail.com>
2016-04-10 20:31:02 +0200

    add mock db

---
+++ Tamagotchi/Tamagotchi.test/MockMyContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tamagotchi_prog.Models;
+using Tamagotchi_prog.Repository;
+
+namespace Tamagotchi.test
+{
+    public class MockMyContext : DbContext
+    {
+        public DbSet<Tamagotchi_prog.Models.Tamagotchi> Tamagotchis { get; set; }
+        public DbSet<RuleSettings> RuleSettings { get; set; }
+        public DbSet<StatusEffect> StatusEffects { get; set; } 
+
+        public MockMyContext()
+            : base("name=DefaultConnection")
+        {
+            System.Data.Entity.Database.SetInitializer(new MyContextInitializer());
+        }
+    }
+}
--- Tamagotchi/Tamagotchi_prog/Models/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi_prog/Models/Tamagotchi.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Dynamic;
 using Tamagotchi_prog.Models.GameActions;
 
 namespace Tamagotchi_prog.Models
